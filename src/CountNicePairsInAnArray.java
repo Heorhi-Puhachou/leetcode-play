12024da163fd9c0dc338133bd609610a4413bbfc
heorhipuhachou <h.puhachou@gmail.com>
2023-11-21 15:53:27 +0100

    CountNicePairsInAnArray v1

---
+++ src/CountNicePairsInAnArray.java
@@ -0,0 +1,78 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class CountNicePairsInAnArray {
+
+    public static void main(String... args) {
+        // System.out.println((new CountNicePairsInAnArray()).isNicePair(413370302, 293999243));
+        int[] a1 = {13, 10, 35, 24, 76};
+        int[] a2 = {42, 11, 1, 97};
+        int[] a3 = {352171103, 442454244, 42644624, 152727101, 413370302, 293999243};
+        int[] a4 = {447130891, 318857960, 348897990, 281595033, 26855862, 465665414, 352380251, 179231820, 308764801};
+        System.out.println((new CountNicePairsInAnArray()).countNicePairs(a4));
+    }
+
+    /**
+     * You are given an array nums that consists of non-negative integers. Let us define rev(x) as the reverse of the non-negative integer x. For example, rev(123) = 321, and rev(120) = 21. A pair of indices (i, j) is nice if it satisfies all of the following conditions:
+     * <p>
+     * 0 <= i < j < nums.length
+     * nums[i] + rev(nums[j]) == nums[j] + rev(nums[i])
+     * <p>
+     * Return the number of nice pairs of indices. Since that number can be too large, return it modulo 109 + 7.
+     * <p>
+     * <p>
+     * <p>
+     * Example 1:
+     * <p>
+     * Input: nums = [42,11,1,97]
+     * Output: 2
+     * Explanation: The two pairs are:
+     * - (0,3) : 42 + rev(97) = 42 + 79 = 121, 97 + rev(42) = 97 + 24 = 121.
+     * - (1,2) : 11 + rev(1) = 11 + 1 = 12, 1 + rev(11) = 1 + 11 = 12.
+     * <p>
+     * Example 2:
+     * <p>
+     * Input: nums = [13,10,35,24,76]
+     * Output: 4
+     */
+
+    public int countNicePairs(int[] nums) {
+        int result = 0;
+        Map<Integer, Integer> pairs = new HashMap<>();
+        int[] altNums = new int[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            altNums[i] = nums[i] - reverse(nums[i]);
+            if (pairs.containsKey(altNums[i])) {
+                pairs.put(altNums[i], pairs.get(altNums[i]) + 1);
+            } else {
+                pairs.put(altNums[i], 1);
+            }
+        }
+        for (Integer integer : pairs.values()) {
+            result = result + calculatePairs(integer);
+        }
+
+        return result;
+    }
+
+    public int calculatePairs(int nodesCount) {
+        int result = 0;
+        for (; nodesCount > 0; nodesCount--) {
+            result = result + (nodesCount - 1);
+        }
+        return result;
+    }
+
+    public int reverse(int input) {
+        if (input < 10) {
+            return input;
+        }
+        int reversedNum = 0;
+
+        while (input != 0) {
+            reversedNum = reversedNum * 10 + input % 10;
+            input = input / 10;
+        }
+        return reversedNum;
+    }
+}

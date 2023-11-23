bdddffa406fc8d1e938b1564d1742bc81f08fd41
heorhipuhachou <h.puhachou@gmail.com>
2023-11-23 16:49:56 +0100

    RegularExpressionMatching v1

--- src/hard/RegularExpressionMatching.java
+++ src/hard/RegularExpressionMatching.java
@@ -1,11 +1,19 @@
 package hard;
 
-import easy.PalindromeNumber;
-
 public class RegularExpressionMatching {
 
     public static void main(String... args) {
-        System.out.println((new RegularExpressionMatching()).isMatch("aa", "a*"));
+        //System.out.println((new RegularExpressionMatching()).isMatch("mississippi", "mis*is*p*."));
+        //System.out.println((new RegularExpressionMatching()).isMatch("mississippi", "mis*is*ip*."));
+        //System.out.println((new RegularExpressionMatching()).isMatch("bbbba", ".*a*a"));
+        //System.out.println((new RegularExpressionMatching()).isMatch("aaa", "aaaa"));
+        //System.out.println((new RegularExpressionMatching()).isMatch("aa", "a*"));
+        //System.out.println((new RegularExpressionMatching()).isMatch("ab", ".*"));
+        //System.out.println((new RegularExpressionMatching()).isMatch("aaa", "a*a"));
+        //System.out.println((new RegularExpressionMatching()).isMatch("ab", ".*c"));
+        //System.out.println((new RegularExpressionMatching()).isMatch("ab", ".*.."));
+        //System.out.println((new RegularExpressionMatching()).isMatch("cdede", ".*de"));
+        System.out.println((new RegularExpressionMatching()).isMatch("aaaaaaaaaaaaaaaaaaab", "a*a*a*a*a*a*a*a*a*a*"));
     }
 
     /**
@@ -37,10 +45,87 @@ public class RegularExpressionMatching {
      * Explanation: ".*" means "zero or more (*) of any character (.)".
      */
     public boolean isMatch(String s, String p) {
-        if (p.length() == 1 && !p.equals(s)) {
+        if (p.equals(".*")) {
+            return true;
+        }
+        return isMatch(s.toCharArray(), simplifyPattern(p).toCharArray(), 0, 0);
+    }
+
+    public boolean isMatch(char[] string, char[] pattern, int stringStart, int patternStart) {
+        boolean stringFinished = string.length <= stringStart;
+        boolean patternFinished = pattern.length == patternStart;
+        boolean lastStringSymbol = stringStart + 1 == string.length;
+        boolean lastPatternSymbol = patternStart + 1 == pattern.length;
+        boolean extendedPattern = pattern.length > patternStart + 1 && pattern[patternStart + 1] == '*';
+        if (lastPatternSymbol && lastStringSymbol &&
+                (string[stringStart] == pattern[patternStart] || pattern[patternStart] == '.')) {
+            return true;
+        }
+        boolean patternCanBeSkipped = patternCanBeSkipped(pattern, patternStart);
+        boolean universalPattern = !patternFinished
+                && pattern[patternStart] == '.'
+                && extendedPattern;
+
+        if (stringFinished && !patternCanBeSkipped) {
+            return false;
+        } else if (stringFinished) {
+            return true;
+        }
+
+        if (patternFinished) {
             return false;
         }
 
+
+        if (universalPattern || (extendedPattern && (string[stringStart] == pattern[patternStart] || pattern[patternStart] == '*'))) {
+            return  //check next symbol with current pattern
+                    isMatch(string, pattern, stringStart + 1, patternStart)
+                            ||
+                            //check next symbol with next part of pattern
+                            isMatch(string, pattern, stringStart + 1, patternStart + 2)
+                            ||
+                            //check current symbol with next part of pattern
+                            isMatch(string, pattern, stringStart, patternStart + 2);
+        } else if (extendedPattern && pattern[patternStart] != string[stringStart]) {
+            //check current symbol with next part of pattern
+            return isMatch(string, pattern, stringStart, patternStart + 2);
+        } else if (!extendedPattern && (pattern[patternStart] == string[stringStart] || pattern[patternStart] == '.')) {
+            //check next symbol with next part of pattern
+            return isMatch(string, pattern, stringStart + 1, patternStart + 1);
+        } else {
+            return false;
+        }
+    }
+
+    private boolean patternCanBeSkipped(char[] pattern, int patternStart) {
+        if ((pattern.length - patternStart) % 2 == 0) {
+            for (int i = patternStart + 1; i < pattern.length; i = i + 2) {
+                if (pattern[i] != '*') {
+                    return false;
+                }
+            }
+        } else {
+            return false;
+        }
         return true;
+    }
+
+    private String simplifyPattern(String p) {
+        char[] pattern = p.toCharArray();
+        boolean changed = false;
+        int i = 0;
+        for (; i < pattern.length - 3; i++) {
+            if ((pattern[i] == pattern[i + 2] || pattern[i + 2] == '.')
+                    && pattern[i + 1] == '*'
+                    && pattern[i + 3] == '*') {
+                changed = true;
+                break;
+            }
+        }
+        if (changed) {
+            return simplifyPattern(p.substring(0, i) + p.substring(i + 2));
+        }
+
+        return p;
     }
 }
